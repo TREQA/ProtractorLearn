a3db8c31753d7c2d019ec9eb2490139ac6c78602
dgurau <dgurau@tremend.ro>
2017-04-07 16:36:26 +0300

    -jenkins

--- runTests.bat
+++ runTests.bat
@@ -1,5 +1,5 @@
-webdriver-manager start
-pause
+webdriver-manager star
+timeout 5
 while ! curl http://localhost:4444/wd/hub/status &>/dev/null; do :; done
 cd C:\Program Files (x86)\Jenkins\workspace\ProtractorLearn\Config
 protractor conf.js
