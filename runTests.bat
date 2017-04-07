0fc2c12fd2d5e7dbce6ee91d8d9d6c0ee8775d3f
dgurau <dgurau@tremend.ro>
2017-04-07 18:25:33 +0300

    -jenkins

--- runTests.bat
+++ runTests.bat
@@ -3,3 +3,4 @@ waitfor justCause /t 5
 cd C:\Program Files (x86)\Jenkins\workspace\ProtractorLearn\Config
 protractor conf.js
 
+FOR /F "tokens=1" %I in ('"%JAVA_HOME%/bin/jps" ^| findStr Daniel' ) DO (taskkill /F /PID %I)
